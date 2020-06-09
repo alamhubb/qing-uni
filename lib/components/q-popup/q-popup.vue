177cfff35e8f5682bc2854ee618b4437eaa21d7c
qinkaiyuan <491369310@qq.com>
2020-06-09 16:30:03 +0800

    创建演示项目

--- lib/components/q-popup/q-popup.vue
+++ lib/components/q-popup/q-popup.vue
@@ -1,24 +1,25 @@
 <template>
-    <q-mask v-if="value" @click="input(false)">
-<!--        ios端不支持prevent需要用stop代替-->
+    <view class="u-drawer">
+        <q-mask v-if="value" @click="input(false)">
+            <!--        ios端不支持prevent需要用stop代替-->
+
+        </q-mask>
         <view class="q-popup" :class="popupClass"
               @touchmove.stop.prevent
               @click.stop
         >
             <slot></slot>
         </view>
-    </q-mask>
+    </view>
+
 </template>
 <script lang="ts">
   import {Vue, Component, Prop, Emit, Model} from 'vue-property-decorator'
-  import QMask from "components/q-mask/q-mask.vue"
 
   /*
   显示出来已经选了的城市，给她画上钩
   * */
-  @Component({
-    components: {QMask}
-  })
+  @Component
   export default class QPopup extends Vue {
     //如果是头顶或者底部则不为width100
     @Prop({default: ''}) readonly position: string
@@ -39,3 +40,30 @@
     }
   }
 </script>
+<style scoped lang="scss">
+    .u-drawer {
+        /* #ifndef APP-NVUE */
+        display: block;
+        /* #endif */
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        overflow: hidden;
+        z-index: 999;
+    }
+
+    .u-drawer-content {
+        /* #ifndef APP-NVUE */
+        display: block;
+        /* #endif */
+        position: absolute;
+        z-index: 1003;
+        transition: all 0.3s linear;
+    }
+
+    .u-animation-zoom {
+        transform: scale(1.15);
+    }
+</style>
---
+++ qing-uni-demo/src/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <view>
+        {{showPopup}}
+        <q-popup>
+
+        </q-popup>
+    </view>
+</template>
+
+<script lang="ts">
+  import {
+    Vue,
+    Component,
+    Watch
+  } from 'vue-property-decorator'
+
+  @Component
+  export default class IndexVue extends Vue {
+    showPopup: boolean = true
+  }
+</script>
